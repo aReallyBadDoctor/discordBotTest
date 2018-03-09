02a19ca45a5b659875b5d156fb1f6b76c6cb70b6
aReallyBadDoctor <37199612+aReallyBadDoctor@users.noreply.github.com>
2018-03-08 22:17:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,12 +20,29 @@ bot.on('ready', function (evt) {
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
-  if
-  elif (message.substring(0, 1) == '!') {
+  if (message.substring(0, 5) == '!toto') {
     var args = message.substring(1).split(' ');
     var cmd = args[0];
     
+    for (i = 0; i < args.length; i++){
+      if args[i] == ':ok_hand:':
+        bot.sendMessage({
+          to channelID,
+          message: "you fuckin got me bro"
+        });
+    }
+    
     args = args.splice(1);
     switch(cmd.toLowerCase()) {
-      case 'africabytoto':
-        
+      case 'help':
+        //this will print the help statement
+        bot.sendMessage({
+          to: channelID,
+          message: "help:/nusage: !toto help/ndisplays this message/ngo:/nusage: !toto go/nsends a meme about a very good song/nadd:/nusage: !toto add <picture_url>/nadds something to the queue/nrecommend:/nusage: !toto recommend/nrecommends a version of a really good song/n"
+        });
+      case 'add':
+        //this will place second argument in judgment queue
+      case 'go':
+        //this will make a meme go
+      case 'recommend'
+        //this will give a recomendation on what to listen to
